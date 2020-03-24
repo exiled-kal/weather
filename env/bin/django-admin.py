0d97bfe124634419068df57101e38bdccaa47971
exiled-kal <tseringkalden1964@gmail.com>
2020-03-24 16:05:18 -0700

    initial commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/admin/Desktop/codingdojo/Python_stack/Django/django_full_stack/ownproject/server/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
